a68ebfb3fd973039b224039608b72a6e844f4ce8
tpoidevin <tom.poidevin@6tm.com>
2019-01-18 21:27:36 +0100

    first commit

--- App.js
+++ App.js
@@ -1,74 +1,100 @@
-import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
-import { Font } from 'expo';
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground
+} from "react-native";
+import { Font } from "expo";
 
 class BackgroundImage extends Component {
+  render() {
+    return (
+      <ImageBackground
+        source={require("./assets/7SYH.gif")}
+        style={styles.backgroundImage}
+      >
+        {this.props.children}
+      </ImageBackground>
+    );
+  }
+}
+class Mycomponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      check: this.props.check
+    };
+  }
+  displayText(){
+    return {
 
-    render() {
-        return (
-            <ImageBackground source={require('./assets/7SYH.gif')}
-                  style={styles.backgroundImage}>
-                  {this.props.children}
-            </ImageBackground>
-        )
     }
+  }
+  render() {
+    return <Text>{this.props.check ? "oui":"non"}</Text>;
+  }
 }
 
 export default class FlatListBasics extends Component {
-  componentDidMount() {
-      Font.loadAsync({
-        'geek': require('./assets/fonts/geek.ttf'),
-      });
-    }
-
   render() {
+    var bonjour = "hello worl";
+
     return (
-      <View style={styles.container}>
-        <BackgroundImage>
-        <View style={ styles.bottomView} >
-              <Text style={styles.textStyle}>AWESOME</Text>
-       </View>
-        </BackgroundImage>
+      <View style={{paddingTop: 50}}>
+        <Mycomponent text={bonjour}
+        check={
+          false
+        } />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-        flex: 2,
-        width: null,
-        height: null,
-        resizeMode: 'cover'
-    },
+  backgroundImage: {
+    flex: 2,
+    width: null,
+    height: null,
+    resizeMode: "cover"
+  },
 
-    text: {
-        width: 'auto',
-        height: 50,
-        color: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        bottom: 0
-    },
+  text: {
+    width: "auto",
+    height: 50,
+    color: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0
+  },
 
-    bottomView:{
-      width: '100%',
-      height: '25%',
-      backgroundColor: '#000',
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      bottom: 0
-    },
+  bottomView: {
+    width: "100%",
+    height: "25%",
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0
+  },
 
-    textStyle:{
-      color: '#fff',
-      fontSize:45
-    },
+  textStyle: {
+    color: "#fff",
+    fontSize: 45
+  },
 
-    container: {
-     flex: 1,
-     paddingTop: 22
-    },
+  container: {
+    paddingTop: 22
+  },
+  RichText: {
+    fontSize: 22,
+    height: 50,
+    backgroundColor: "#BADA55",
+    borderWidth: 1,
+    borderColor: "#333"
+  }
 });
